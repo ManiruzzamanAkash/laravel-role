73d1998f1bc823b2b4c9b796240d02da20641484
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2021-11-13 23:13:55 +0600

    Create default admin and seeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = Admin::where('username', 'superadmin')->first();
+
+        if (is_null($admin)) {
+            $admin           = new Admin();
+            $admin->name     = "Super Admin";
+            $admin->email    = "superadmin@example.com";
+            $admin->username = "superadmin";
+            $admin->password = Hash::make('12345678');
+            $admin->save();
+        }
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(AdminSeeder::class);
         $this->call(RolePermissionSeeder::class);
     }
 }
