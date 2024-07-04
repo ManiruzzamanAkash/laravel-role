28cfb5a3f98ab46f41823e32576c699b4f1104e3
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-07-05 01:16:59 +0600

    Implement confirm before delete

--- resources/views/backend/pages/roles/index.blade.php
+++ resources/views/backend/pages/roles/index.blade.php
@@ -2,7 +2,7 @@
 @extends('backend.layouts.master')
 
 @section('title')
-Role Page - Admin Panel
+{{ __('Roles - Admin Panel') }}
 @endsection
 
 @section('styles')
@@ -13,7 +13,6 @@ Role Page - Admin Panel
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
 @endsection
 
-
 @section('admin-content')
 
 <!-- page title area start -->
@@ -21,10 +20,10 @@ Role Page - Admin Panel
     <div class="row align-items-center">
         <div class="col-sm-6">
             <div class="breadcrumbs-area clearfix">
-                <h4 class="page-title pull-left">Roles</h4>
+                <h4 class="page-title pull-left">{{ __('Roles') }}</h4>
                 <ul class="breadcrumbs pull-left">
-                    <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                    <li><span>All Roles</span></li>
+                    <li><a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a></li>
+                    <li><span>{{ __('All Roles') }}</span></li>
                 </ul>
             </div>
         </div>
@@ -41,9 +40,9 @@ Role Page - Admin Panel
         <div class="col-12 mt-5">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title float-left">Roles List</h4>
+                    <h4 class="header-title float-left">{{ __('Roles') }}</h4>
                     <p class="float-right mb-2">
-                        @if (Auth::guard('admin')->user()->can('role.create'))
+                        @if (Auth::user()->can('role.create'))
                             <a class="btn btn-primary text-white" href="{{ route('admin.roles.create') }}">Create New Role</a>
                         @endif
                     </p>
@@ -53,33 +52,33 @@ Role Page - Admin Panel
                         <table id="dataTable" class="text-center">
                             <thead class="bg-light text-capitalize">
                                 <tr>
-                                    <th width="5%">Sl</th>
-                                    <th width="10%">Name</th>
-                                    <th width="60%">Permissions</th>
-                                    <th width="15%">Action</th>
+                                    <th width="5%">{{ __('Sl') }}</th>
+                                    <th width="10%">{{ __('Name') }}</th>
+                                    <th width="60%">{{ __('Permissions') }}</th>
+                                    <th width="15%">{{ __('Action') }}</th>
                                 </tr>
                             </thead>
                             <tbody>
                                @foreach ($roles as $role)
                                <tr>
-                                    <td>{{ $loop->index+1 }}</td>
+                                    <td>{{ $loop->index + 1 }}</td>
                                     <td>{{ $role->name }}</td>
                                     <td>
-                                        @foreach ($role->permissions as $perm)
+                                        @foreach ($role->permissions as $permission)
                                             <span class="badge badge-info mr-1">
-                                                {{ $perm->name }}
+                                                {{ $permission->name }}
                                             </span>
                                         @endforeach
                                     </td>
                                     <td>
-                                        @if (Auth::guard('admin')->user()->can('admin.edit'))
+                                        @if (auth::user()->can('admin.edit'))
                                             <a class="btn btn-success text-white" href="{{ route('admin.roles.edit', $role->id) }}">Edit</a>
                                         @endif
 
-                                        @if (Auth::guard('admin')->user()->can('admin.edit'))
-                                            <a class="btn btn-danger text-white" href="{{ route('admin.roles.destroy', $role->id) }}"
-                                            onclick="event.preventDefault(); document.getElementById('delete-form-{{ $role->id }}').submit();">
-                                                Delete
+                                        @if (auth::user()->can('admin.edit'))
+                                            <a class="btn btn-danger text-white" href="javascript:void(0);"
+                                                onclick="event.preventDefault(); if(confirm('Are you sure to delete?')) { document.getElementById('delete-form-{{ $role->id }}').submit(); }">
+                                                {{ __('Delete') }}
                                             </a>
 
                                             <form id="delete-form-{{ $role->id }}" action="{{ route('admin.roles.destroy', $role->id) }}" method="POST" style="display: none;">
@@ -97,11 +96,9 @@ Role Page - Admin Panel
             </div>
         </div>
         <!-- data table end -->
-        
     </div>
 </div>
 @endsection
-
 
 @section('scripts')
      <!-- Start datatable js -->
@@ -112,14 +109,10 @@ Role Page - Admin Panel
      <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
      
      <script>
-         /*================================
-        datatable active
-        ==================================*/
         if ($('#dataTable').length) {
             $('#dataTable').DataTable({
                 responsive: true
             });
         }
-
      </script>
 @endsection
