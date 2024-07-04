e93eb4f8b72b3964c3d17d03e0859f6efc02a78c
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-07-05 01:18:06 +0600

    Refactor Dashboard Controller

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -1,36 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class DashboardController extends Controller
 {
-    public $user;
-
-    public function __construct()
-    {
-        $this->middleware(function ($request, $next) {
-            $this->user = Auth::guard('admin')->user();
-            return $next($request);
-        });
-    }
-
-
     public function index()
     {
-        if (is_null($this->user) || !$this->user->can('dashboard.view')) {
-            abort(403, 'Sorry !! You are Unauthorized to view dashboard !');
-        }
+        $this->checkAuthorization(auth()->user(), ['dashboard.view']);
 
-        $total_roles = count(Role::select('id')->get());
-        $total_admins = count(Admin::select('id')->get());
-        $total_permissions = count(Permission::select('id')->get());
-        return view('backend.pages.dashboard.index', compact('total_admins', 'total_roles', 'total_permissions'));
+        return view(
+            'backend.pages.dashboard.index',
+            [
+                'total_admins' => Admin::count(),
+                'total_roles' => Role::count(),
+                'total_permissions' => Permission::count(),
+            ]
+        );
     }
 }
