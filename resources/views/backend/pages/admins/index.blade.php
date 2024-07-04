d18b8f5ed0eff6c860b6061480ef2499d5e344de
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-07-05 01:22:18 +0600

    Added confirm before delete admin

--- resources/views/backend/pages/admins/index.blade.php
+++ resources/views/backend/pages/admins/index.blade.php
@@ -1,8 +1,7 @@
-
 @extends('backend.layouts.master')
 
 @section('title')
-Admins - Admin Panel
+    {{ __('Admins - Admin Panel') }}
 @endsection
 
 @section('styles')
@@ -13,7 +12,6 @@ Admins - Admin Panel
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
 @endsection
 
-
 @section('admin-content')
 
 <!-- page title area start -->
@@ -21,10 +19,10 @@ Admins - Admin Panel
     <div class="row align-items-center">
         <div class="col-sm-6">
             <div class="breadcrumbs-area clearfix">
-                <h4 class="page-title pull-left">Admins</h4>
+                <h4 class="page-title pull-left">{{ __('Admins') }}</h4>
                 <ul class="breadcrumbs pull-left">
-                    <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                    <li><span>All Admins</span></li>
+                    <li><a href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a></li>
+                    <li><span>{{ __('All Admins') }}</span></li>
                 </ul>
             </div>
         </div>
@@ -41,10 +39,12 @@ Admins - Admin Panel
         <div class="col-12 mt-5">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title float-left">Admins List</h4>
+                    <h4 class="header-title float-left">{{ __('Admins') }}</h4>
                     <p class="float-right mb-2">
-                        @if (Auth::guard('admin')->user()->can('admin.edit'))
-                            <a class="btn btn-primary text-white" href="{{ route('admin.admins.create') }}">Create New Admin</a>
+                        @if (auth()->user()->can('admin.edit'))
+                            <a class="btn btn-primary text-white" href="{{ route('admin.admins.create') }}">
+                                {{ __('Create New Admin') }}
+                            </a>
                         @endif
                     </p>
                     <div class="clearfix"></div>
@@ -53,11 +53,11 @@ Admins - Admin Panel
                         <table id="dataTable" class="text-center">
                             <thead class="bg-light text-capitalize">
                                 <tr>
-                                    <th width="5%">Sl</th>
-                                    <th width="10%">Name</th>
-                                    <th width="10%">Email</th>
-                                    <th width="40%">Roles</th>
-                                    <th width="15%">Action</th>
+                                    <th width="5%">{{ __('Sl') }}</th>
+                                    <th width="10%">{{ __('Name') }}</th>
+                                    <th width="10%">{{ __('Email') }}</th>
+                                    <th width="40%">{{ __('Roles') }}</th>
+                                    <th width="15%">{{ __('Action') }}</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -74,15 +74,16 @@ Admins - Admin Panel
                                         @endforeach
                                     </td>
                                     <td>
-                                        @if (Auth::guard('admin')->user()->can('admin.edit'))
+                                        @if (auth()->user()->can('admin.edit'))
                                             <a class="btn btn-success text-white" href="{{ route('admin.admins.edit', $admin->id) }}">Edit</a>
                                         @endif
                                         
-                                        @if (Auth::guard('admin')->user()->can('admin.delete'))
-                                        <a class="btn btn-danger text-white" href="{{ route('admin.admins.destroy', $admin->id) }}"
-                                        onclick="event.preventDefault(); document.getElementById('delete-form-{{ $admin->id }}').submit();">
-                                            Delete
+                                        @if (auth()->user()->can('admin.delete'))
+                                        <a class="btn btn-danger text-white" href="javascript:void(0);"
+                                        onclick="event.preventDefault(); if(confirm('Are you sure you want to delete?')) { document.getElementById('delete-form-{{ $admin->id }}').submit(); }">
+                                            {{ __('Delete') }}
                                         </a>
+
                                         <form id="delete-form-{{ $admin->id }}" action="{{ route('admin.admins.destroy', $admin->id) }}" method="POST" style="display: none;">
                                             @method('DELETE')
                                             @csrf
@@ -98,11 +99,9 @@ Admins - Admin Panel
             </div>
         </div>
         <!-- data table end -->
-        
     </div>
 </div>
 @endsection
-
 
 @section('scripts')
      <!-- Start datatable js -->
@@ -113,14 +112,10 @@ Admins - Admin Panel
      <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
      
      <script>
-         /*================================
-        datatable active
-        ==================================*/
         if ($('#dataTable').length) {
             $('#dataTable').DataTable({
                 responsive: true
             });
         }
-
      </script>
 @endsection
