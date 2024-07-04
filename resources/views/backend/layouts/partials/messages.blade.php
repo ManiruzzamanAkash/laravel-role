8528090c6661d7853671476853cff779cde2448e
makashdev <man.akash.dev@gmail.com>
2024-07-05 00:10:57 +0600

    Add collapsible on role

--- resources/views/backend/layouts/partials/messages.blade.php
+++ resources/views/backend/layouts/partials/messages.blade.php
@@ -1,25 +1,30 @@
 @if ($errors->any())
-    <div class="alert alert-danger">
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <div>
             @foreach ($errors->all() as $error)
                 <p>{{ $error }}</p>
             @endforeach
         </div>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if (Session::has('success'))
-    <div class="alert alert-success">
-        <div>
-            <p>{{ Session::get('success') }}</p>
-        </div>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <span>{{ Session::get('success') }}</span>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 
 @if (Session::has('error'))
-    <div class="alert alert-danger">
-        <div>
-            <p>{{ Session::get('error') }}</p>
-        </div>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <span>{{ Session::get('error') }}</span>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
