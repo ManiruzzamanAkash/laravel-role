897c5fc53e657b9791d25219e7334d5eb13a2f6c
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-03-26 11:13:46 +0600

    Add dark mode support

--- resources/views/components/filters/date-filter.blade.php
+++ resources/views/components/filters/date-filter.blade.php
@@ -10,7 +10,7 @@
     <button
         id="dropdownDefaultButton"
         data-dropdown-toggle="dropdown"
-        class="text-gray-800 hover:text-white bg-gray-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="text-gray-800 dark:text-gray-100 hover:text-white bg-gray-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
     >
         <svg
